33b6a064e9b44423e63899114846e2fdd696a902
Brandon Hu <brandonhu32@g.ucla.edu>
2026-09-24 16:41:12 -0700

    Managed to get map working

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,27 +1,29 @@
 import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
 
-const Map = ({ center, zoom }) => {
+const Map = ({
+  center = {
+    lat: 42.3265,
+    lng: -122.8756
+  },
+  zoom = 6
+}) => {
   return (
     <div className="map">
-        <GoogleMapReact
-            bootstrapURLKeys={{ key: 'AIzaSyDReWRyzloepWSdcWDZbhcWTN_iEmFHWSI' }}
-            defaultCenter={ center }
-            defaultZoom={ zoom }
-        >
-
-        </GoogleMapReact>
-      
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: 'AIzaSyDReWRyzloepWSdcWDZbhcWTN_iEmFHWSI'
+        }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        <LocationMarker
+          lat={center.lat}
+          lng={center.lng}
+        />
+      </GoogleMapReact>
     </div>
   )
 }
-
-Map.defaultProps = {
-    center: {
-        lat: 42.3265,
-        lng: -122.8756
-    },
-    zoom: 6
-}
-
 
 export default Map
