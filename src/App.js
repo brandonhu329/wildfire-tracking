0d14d5984abd78dc71667ccd12d71dcb560600ac
Brandon Hu <brandonhu32@g.ucla.edu>
2026-09-25 21:22:14 -0700

    Added loading words

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useState, useEffect } from 'react'
 import Map from './components/Map'
 
 function App() {
+  const [eventData, setEventData] = useState([])
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await fetch(
+        'https://eonet.gsfc.nasa.gov/api/v2.1/events'
+      )
+
+      const data = await res.json()
+
+      console.log('NASA response:', data)
+
+      setEventData(data.events)
+    }
+
+    fetchEvents()
+  }, [])
+
+  console.log('eventData:', eventData)
+
   return (
     <div>
-      <Map /> 
+      <Map />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
